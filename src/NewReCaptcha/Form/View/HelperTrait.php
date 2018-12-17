c598426487d33a58df0a4331da2eefd1c9c0f448
Benoit Asselin <basselin@users.noreply.github.com>
2018-12-17 22:58:11 +0100

    Create HelperTrait.php

---
+++ src/NewReCaptcha/Form/View/HelperTrait.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @link      https://github.com/basselin/zf2-new-recaptcha
+ * @copyright (c) 2015-2018, Benoit Asselin contact(at)161.io
+ * @license   MIT License
+ */
+
+namespace NewReCaptcha\Form\View;
+
+use NewReCaptcha\Form\View\Helper\FormNewReCaptcha;
+use Zend\Form\ElementInterface;
+
+/**
+ * Helper trait for auto-completion of code in modern IDEs.
+ *
+ * @method FormNewReCaptcha|string formNewReCaptcha(ElementInterface $element = null, $withApiJs = true, $theme = null)
+ */
+trait HelperTrait
+{
+}
